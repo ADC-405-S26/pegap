af53981a4bf213792197eb7b6a4ff61afd91dd9a
OReece <reeceo@lawrence.edu>
2026-05-28 21:08:15 -0500

    fixed issues with dataset in r folder

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' A dataset to be used to practice with the porfolio analysis functions.
+#'
+#'
+#'
+#' @format ## `code_script`
+#' A data frame with 5 rows and 5 columns:
+#' \describe{
+#'   \item{fund}{Name of the fund (character)}
+#'   \item{vintage_year}{Year the fund began investing (integer)}
+#'   \item{commitment}{Total capital committed in USD (numeric)}
+#'   \item{called}{Capital called to date in USD (numeric)}
+#'   \item{recallable}{Recallable distributions in USD (numeric)}
+#' }
+"code_script"
